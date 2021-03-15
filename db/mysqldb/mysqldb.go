e3a58ea0d5bb0e672a57cde4deb49ba63c0997a8
thor <thor@localhost>
2021-03-15 12:14:20 +0800

    mysql db 删除 log

--- db/mysqldb/mysqldb.go
+++ db/mysqldb/mysqldb.go
@@ -3,7 +3,6 @@ package mysqldb
 import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/keyuquan/phoenix/log"
 	"time"
 )
 
@@ -16,9 +15,5 @@ func OpenDB(path string) (*sql.DB, error) {
 	DB.SetConnMaxLifetime(time.Minute * 3)
 	DB.SetMaxOpenConns(10)
 	DB.SetMaxIdleConns(10)
-	if err := DB.Ping(); err != nil {
-		log.Debug("open database fail ...")
-	}
-	log.Debug("open database success ...")
 	return DB, err
 }
